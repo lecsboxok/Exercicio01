62786a23620f18e212c1f4fa62b8a663be246933
lecsboxok <133366992+lecsboxok@users.noreply.github.com>
2024-04-12 15:04:55 -0300

    Initial commit

---
+++ Exercicio1/Exercicio1.cpp
@@ -0,0 +1,93 @@
+//1. A empresa TERMAL conhecida pelos seus serviços de instalação de banheiras solicitou um sistema que para somar os serviços feitos pelos seus funcionários.Os serviços são :
+//		Instalação – R$300.00
+//		Limpeza – R$150.00
+//		Manutenção – 499.99
+//Quando o cliente deverá pagar para a empresa ?
+
+//#include <iostream>
+//
+//using namespace std;
+//int main() {
+//	float limpeza = 150.00;
+//	float intalacao = 300.00;
+//	float manutencao = 499.99;
+//
+//	float valor = limpeza + intalacao + manutencao;
+//
+//	cout << "O cliente deverá pagar R$";
+//	cout << valor;
+//
+//	return 0;
+//}
+
+//2. Joaquim trabalha em uma empresa e recebe R$35.00 reais por hora e trabalha 8 horas por dia.Ele solicitou um programa que calcule quanto ao final de um dia ele deve receber
+
+//#include <iostream>
+//
+//using namespace std;
+//int main() {
+//
+//	float recebeHora = 35;
+//	int horasTrabalhadas = 8;
+//
+//	float recebeDia = recebeHora * horasTrabalhadas;
+//
+//	cout << "O Joaquim deverá receber R$";
+//	cout << recebeDia;
+//	cout << " pelo dia trabalhado.";
+//
+//	return 0;
+//}
+
+//3. Cada caixa de bombom vem 27 unidades, Pamela comprou 16 unidades da caixa de bombom, quantos bombons ela tem ?
+
+//#include <iostream>
+//
+//using namespace std;
+//int main() {
+//	int caixa = 27;
+//	int Pamela = 16;
+//
+//	int bombonsCaixa = caixa - Pamela;
+//
+//	cout << "Na caixa de bombom tem ";
+//	cout << bombonsCaixa;
+//	cout << " bombons";
+//
+//	return 0;
+//}
+
+//4. Uma loja vendeu 300 garrafas de água, cada caixa vem 20 garrafas, quantas caixas eles usaram ?
+//#include <iostream>
+//
+//using namespace std;
+//int main() {
+//
+//	int garrafas = 300;
+//	int caixa = 20;
+//
+//	int quantidadeDeCaixas = garrafas / caixa;
+//
+//	cout << "Eles usaram ";
+//	cout << quantidadeDeCaixas;
+//	cout << " caixas";
+//
+//	return 0;
+//}
+
+// 5. Uma loja comprou 120 camisetas e vendeu 80 delas, cada uma custou R$ 30.00.E logo em seguida comprou mais 50 camisetas, quantas camisetas ele tem agora e qual foi seu faturamento ?
+
+//#include <iostream>
+//
+//using namespace std;
+//int main() {
+//
+//	int loja = (120 - 80) + 50;
+//	float faturamento = 80 * 30;
+//
+//	cout << "A loja tem ";
+//	cout << loja;
+//	cout << " camisetas";
+//	cout << "\nE o seu faturamento foi de R$";
+//	cout << faturamento;
+//}
